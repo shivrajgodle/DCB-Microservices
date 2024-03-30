63f8a4e4e9e453b732d9e670911c6ae8e8598c66
SHIVRAJ <83442544+shivrajgodle@users.noreply.github.com>
2024-03-30 15:22:06 +0530

    added circuit breaker for order service

--- OrderService/src/main/java/com/shivraj/OrderService/external/client/PaymentService.java
+++ OrderService/src/main/java/com/shivraj/OrderService/external/client/PaymentService.java
@@ -1,15 +1,22 @@
 package com.shivraj.OrderService.external.client;
 
+import com.shivraj.OrderService.exception.CustomException;
 import com.shivraj.OrderService.external.client.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker(name = "external",fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE/payment")
 public interface PaymentService {
 
     @PostMapping
     public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest PaymentRequest);
+
+    default void fallback(Exception e){
+        throw new CustomException("Payment service is not acceptable",500,"UNAVAILABLE");
+    }
 
 }
